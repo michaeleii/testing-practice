fd717dc6e394d1a08dd9a71558619431c2813369
Michael Lei <michaelleie0ca@gmail.com>
2023-03-19 21:20:05 -0700

    Finish calculator tests

---
+++ calculator.test.ts
@@ -0,0 +1,27 @@
+import Calculator from "./calculator";
+
+test("1 + 1 should be 2", () => {
+	expect(Calculator.add(1, 1)).toBe(2);
+});
+
+test("5 - 2 should be 3", () => {
+	expect(Calculator.subtract(5, 2)).toBe(3);
+});
+
+test("2 - 5 should be -3", () => {
+	expect(Calculator.subtract(2, 5)).toBe(-3);
+});
+
+test("2 * 5 should be 10", () => {
+	expect(Calculator.multiply(2, 5)).toBe(10);
+});
+
+test("10 / 2 should be 5", () => {
+	expect(Calculator.divide(10, 2)).toBe(5);
+});
+
+test("10 / 0 should throw an error", () => {
+	expect(() => {
+		Calculator.divide(10, 0);
+	}).toThrow();
+});
---
+++ calculator.ts
@@ -0,0 +1,17 @@
+export default class Calculator {
+	static add(a: number, b: number): number {
+		return a + b;
+	}
+	static subtract(a: number, b: number): number {
+		return a - b;
+	}
+	static multiply(a: number, b: number): number {
+		return a * b;
+	}
+	static divide(a: number, b: number): number {
+		if (b === 0) {
+			throw new Error("Cannot divide by zero");
+		}
+		return a / b;
+	}
+}
