7caebd4e3d3d30d1e25a6d877d31dfd6e3126c95
Michael Lei <michaelleie0ca@gmail.com>
2023-03-19 21:08:54 -0700

    Finish capitalize test

---
+++ capitalize.test.ts
@@ -0,0 +1,9 @@
+import { capitalize } from "./capitalize";
+
+test("noodle should be Noodle", () => {
+	expect(capitalize("noodle")).toBe("Noodle");
+});
+
+test("Pizza should be Pizza", () => {
+	expect(capitalize("Pizza")).toBe("Pizza");
+});
